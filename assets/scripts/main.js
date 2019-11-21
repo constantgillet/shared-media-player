f04653d21f787961a94a58d8e080ca837daa64a0
constant gillet <constant.gillet@hetic.net>
2019-11-21 21:20:56 +0100

    adding notification system

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,34 @@
 //Connecting to socket io
 const socket = io.connect('http://localhost:8080')
 
-function surligne(champ, erreur)
+//Notification class
+class Notification
 {
-   if(erreur)
-      champ.style.backgroundColor = "#fba";
-   else
-      champ.style.backgroundColor = "";
+   constructor(_element)
+   {
+      this.element = _element
+      this.elementMessage = this.element.querySelector('.js-notificationMessage')
+   }
+   
+   displayNotification(type, message)
+   {  
+      //We modify the content the message
+      this.elementMessage.innerHTML = message
+      this.element.classList.remove('notificationDisplay')
+      
+      //return undefinded to replay animation
+      void this.element.offsetWidth
+      
+      //If the type is success we replace notification--error by notification--success
+      if(type == 'success')
+         this.element.classList.replace('notification--error', 'notification--success')
+      else
+         this.element.classList.replace('notification--success', 'notification--error')
+
+      //We add the class to display the div of the notification
+      this.element.classList.add('notificationDisplay')
+   }
 }
 
-//On controle que c'est bien une URL video
-
-
-//On envoie la vidéo au serveur qui le stocket
-
+//Notification object
+const notification = new Notification(document.querySelector('.js-notification'))
